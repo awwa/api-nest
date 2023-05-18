99365d227884ffc8a7522c1510b5dfc7af894072
awwa500@gmail.com <awwa500@gmail.com>
2023-05-19 07:58:31 +0900

    リソース追加

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
+import { Request } from 'express';
+
+@Controller('cars')
+export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
+  @Post()
+  create(@Body() createCarDto: CreateCarDto) {
+    return this.carsService.create(createCarDto);
+  }
+
+  @Get()
+  findAll(@Req() request: Request): string {
+    console.log(request.query);
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.carsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCarDto: UpdateCarDto) {
+    return this.carsService.update(+id, updateCarDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.carsService.remove(+id);
+  }
+}
