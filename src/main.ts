56abac9fde04f6517e71260d8bc6bcc1d01bc37d
awwa500@gmail.com <awwa500@gmail.com>
2023-05-27 21:31:20 +0900

    change to typescript-axios

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './all-exception.filter';
+import { SwaggerModule } from '@nestjs/swagger';
+import OpenAPIDocument from './openapi/openapi-document';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +10,7 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
+  SwaggerModule.setup('api', app, OpenAPIDocument(app));
   await app.listen(3000);
 }
 bootstrap();
